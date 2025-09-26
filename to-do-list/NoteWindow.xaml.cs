cbc6496c9823d4e35df8e3deec1ff1e69c0d817b
DrNormR <normanr@newmanu.edu>
2025-09-25 19:17:17 -0500

    pin icon fixed

--- to-do-list/NoteWindow.xaml.cs
+++ to-do-list/NoteWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Linq;
 
 namespace to_do_list
 {
@@ -103,6 +104,82 @@ namespace to_do_list
             if (sender is Button button && button.DataContext is NoteItem item)
             {
                 _note.Items.Remove(item);
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1 && sender is TextBlock textBlock)
+            {
+                var parent = textBlock.Parent as Grid;
+                if (parent != null)
+                {
+                    var textBox = parent.Children.OfType<TextBox>().FirstOrDefault();
+                    if (textBox != null)
+                    {
+                        textBlock.Visibility = Visibility.Collapsed;
+                        textBox.Visibility = Visibility.Visible;
+                        textBox.Focus();
+                        textBox.SelectAll();
+                    }
+                }
+            }
+        }
+
+        private void EditTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                var parent = textBox.Parent as Grid;
+                if (parent != null)
+                {
+                    var textBlock = parent.Children.OfType<TextBlock>().FirstOrDefault();
+                    if (textBlock != null)
+                    {
+                        textBox.Visibility = Visibility.Collapsed;
+                        textBlock.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+        }
+
+        private void EditTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (e.Key == Key.Enter)
+                {
+                    var parent = textBox.Parent as Grid;
+                    if (parent != null)
+                    {
+                        var textBlock = parent.Children.OfType<TextBlock>().FirstOrDefault();
+                        if (textBlock != null)
+                        {
+                            textBox.Visibility = Visibility.Collapsed;
+                            textBlock.Visibility = Visibility.Visible;
+                        }
+                    }
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.Escape)
+                {
+                    if (textBox.DataContext is NoteItem item)
+                    {
+                        // Revert changes
+                        textBox.Text = item.Text;
+                    }
+                    var parent = textBox.Parent as Grid;
+                    if (parent != null)
+                    {
+                        var textBlock = parent.Children.OfType<TextBlock>().FirstOrDefault();
+                        if (textBlock != null)
+                        {
+                            textBox.Visibility = Visibility.Collapsed;
+                            textBlock.Visibility = Visibility.Visible;
+                        }
+                    }
+                    e.Handled = true;
+                }
             }
         }
     }
